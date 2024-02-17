fb696f7803e7e3bf6b962370d28d56d72e69e555
Waldo Panozo <waldo.panozo@gmail.com>
2024-02-17 16:19:34 -0400

    fix: when you dont receive indexes failures

--- views/_dropIndexes.php
+++ views/_dropIndexes.php
@@ -2,6 +2,8 @@
 /* @var $table string the name table */
 /* @var $indexes array the foreign keys */
 
-foreach ($indexes as $key => $index): ?>
-        $this->dropIndex('<?= $key ?>', $this->tableName);
-<?php endforeach;
+if($indexes){
+        foreach ($indexes as $key => $index): ?>
+                $this->dropIndex('<?= $key ?>', $this->tableName);
+        <?php endforeach;
+}
