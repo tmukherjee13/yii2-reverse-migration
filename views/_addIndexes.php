5af05875ffc7b96ff16be060515c7680bbe97498
Waldo Panozo <waldo.panozo@gmail.com>
2024-02-21 08:00:46 -0400

    fix: when you dont receive indexes failures (#19)

--- views/_addIndexes.php
+++ views/_addIndexes.php
@@ -8,14 +8,16 @@
 
 //dd( $indexes );
 
-foreach ($indexes as $index):
-if(is_string($index['Column_name'])): ?>
-<?php if($index['Non_unique'] == 0): ?>
-        $this->createIndex('<?= str_replace('_UNIQUE', '', $index['Key_name'])?>', $this->tableName, '<?= $index['Column_name'] ?>', true);
-<?php else: ?>
-        $this->createIndex('<?= $index['Key_name']  ?>', $this->tableName, '<?= $index['Column_name'] ?>');
-<?php endif; ?>
-<?php else: ?>
-        $this->createIndex('<?= $index['Key_name']  ?>', $this->tableName, ['<?= implode("','",$index['Column_name']) ?>']);
-<?php endif; ?>
-<?php endforeach;
+if($indexes){
+        foreach ($indexes as $index):
+        if(is_string($index['Column_name'])): ?>
+        <?php if($index['Non_unique'] == 0): ?>
+                $this->createIndex('<?= str_replace('_UNIQUE', '', $index['Key_name'])?>', $this->tableName, '<?= $index['Column_name'] ?>', true);
+        <?php else: ?>
+                $this->createIndex('<?= $index['Key_name']  ?>', $this->tableName, '<?= $index['Column_name'] ?>');
+        <?php endif; ?>
+        <?php else: ?>
+                $this->createIndex('<?= $index['Key_name']  ?>', $this->tableName, ['<?= implode("','",$index['Column_name']) ?>']);
+        <?php endif; ?>
+        <?php endforeach;
+}
